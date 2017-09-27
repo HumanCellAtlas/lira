378856fd973b36a781d8b3ab3cf76d3fc52dd0ec
Ambrose J Carr <mail@ambrosejcarr.com>
2017-09-27 19:14:58 -0400

    Added config class to validate config.json during startup

--- green_box/__init__.py
+++ green_box/__init__.py
@@ -4,23 +4,20 @@
 Green box description FIXME: elaborate
 """
 
-import os, sys, json, time, logging
-from datetime import datetime, timedelta
-
-from flask import Flask, request, redirect, jsonify
+import os
+import json
+import logging
 import connexion
 from connexion.resolver import RestyResolver
-
+from .config import ListenerConfig
 logging.basicConfig(level=logging.DEBUG)
 
+
 app = connexion.App(__name__)
-app.app.config['MAX_CONTENT_LENGTH'] = 10 * 1000
 
 config_path = os.environ['listener_config']
 with open(config_path) as f:
-    config = json.load(f)
-    for key in config:
-        app.app.config[key] = config[key]
+    app.app.config = ListenerConfig(json.load(f), app.app.config)
 
 resolver = RestyResolver("green_box.api", collection_endpoint_name="list")
 app.add_api('../green_box.yml', resolver=resolver, validate_responses=True)
---
+++ test/test_validate_config.py
@@ -0,0 +1,78 @@
+import unittest
+import json
+from copy import deepcopy
+import green_box.config as validate_config
+
+
+class TestStartupVerification(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """load the config file"""
+        with open('config.json', 'rb') as f:
+            cls.correct_test_config = json.load(f)
+
+    def test_correct_config_throws_no_errors(self):
+        config = deepcopy(self.correct_test_config)
+
+        try:  # make sure ListenerConfig executes
+            result = validate_config.ListenerConfig(config)
+        except BaseException as exception:
+            self.fail(
+                'ListenerConfig constructor raised an exception: {exception}'
+                .format(exception=exception))
+
+        # check correct type
+        self.assertIsInstance(result, validate_config.ListenerConfig)
+
+    def test_config_missing_required_field_throws_value_error(self):
+
+        def delete_wdl_field(field_name):
+            """return a config object whose first wdl config is missing a field"""
+            config = deepcopy(self.correct_test_config)
+            del config['wdls'][0][field_name]
+            return config
+
+        mangled_config = delete_wdl_field('subscription_id')
+        self.assertRaises(ValueError, validate_config.ListenerConfig, mangled_config)
+        mangled_config = delete_wdl_field('workflow_name')
+        self.assertRaises(ValueError, validate_config.ListenerConfig, mangled_config)
+        mangled_config = delete_wdl_field('wdl_link')
+        self.assertRaises(ValueError, validate_config.ListenerConfig, mangled_config)
+        mangled_config = delete_wdl_field('wdl_default_inputs_link')
+        self.assertRaises(ValueError, validate_config.ListenerConfig, mangled_config)
+        mangled_config = delete_wdl_field('wdl_deps_link')
+        self.assertRaises(ValueError, validate_config.ListenerConfig, mangled_config)
+
+    def test_config_duplicate_wdl_raises_value_error(self):
+
+        def add_duplicate_wdl_definition():
+            """add the first wdl definition to the end of the 'wdls' json section"""
+            config = deepcopy(self.correct_test_config)
+            config['wdls'].append(config['wdls'][0])
+            return config
+
+        mangled_config = add_duplicate_wdl_definition()
+        self.assertRaises(ValueError, validate_config.ListenerConfig, mangled_config)
+
+    def test_listener_config_exposes_all_methods_requested_in_notifications(self):
+        # test that the call smade in notifications refer to attributes that
+        # ListenerConfig exposes
+        config = validate_config.ListenerConfig(self.correct_test_config)
+        requested_listener_attributes = [
+            'notification_token', 'wdls', 'cromwell_url', 'cromwell_user',
+            'cromwell_password', 'MAX_CONTENT_LENGTH']
+        for attr in requested_listener_attributes:
+            self.assertTrue(hasattr(config, attr), 'missing attribute %s' % attr)
+
+        # get an example wdl to test
+        wdl = config.wdls[0]
+        requested_wdl_attributes = [
+            'subscription_id', 'wdl_link', 'workflow_name', 'wdl_default_inputs_link',
+            'wdl_deps_link', 'options_link']
+        for attr in requested_wdl_attributes:
+            self.assertTrue(hasattr(wdl, attr), 'missing attribute %s' % attr)
+
+
+if __name__ == "__main__":
+    unittest.main()
