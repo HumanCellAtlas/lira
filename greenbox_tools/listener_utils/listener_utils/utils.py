e3838d7c98c61dc6901bcf7d1ba0890928a916f0
Rex Wang <chengche@broadinstitute.org>
2017-10-16 15:55:41 -0400

    Refactor green_box_utils, add elaborate docstring, add unittests(196 241)

---
+++ greenbox_tools/listener_utils/listener_utils/utils.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import logging
+from requests.auth import HTTPBasicAuth
+from flask import make_response
+from google.cloud import storage
+
+
+def get_filename_from_gs_link(link):
+    """
+    Get the filename corresponding to a google_storage link.
+    :param str link: A string of google cloud storage link.
+    :return str: A string of filename.
+    """
+    return link.split('/')[-1]
+
+
+def parse_bucket_blob_from_gs_link(path):
+    """Utility to split a google storage path into bucket + blob name.
+    :param str path: A string of google cloud storage path (must have gs:// prefix)
+    :return str: A string of bucket name.
+    :return str: A string of blob. name
+    """
+    if not path.startswith('gs://'):
+        raise ValueError('%s path is not a valid code review')
+    prefix, _, bucket, *blob = path.split('/')
+
+    return bucket, '/'.join(blob)
+
+
+def response_with_server_header(body, status=200):
+    """Add information of server to header.
+    :param body: HTTP response body content.
+    :param int status: HTTP response status code.
+    :return: HTTP response with information of server in header.
+    """
+    response = make_response(json.dumps(body), status)
+    response.headers['Server'] = 'Secondary Analysis Service'
+    response.headers['Content-type'] = 'application/json'
+    return response
+
+
+def is_authenticated(args, token):
+    """Check if is authenticated.
+    :param dict args: A dictionary of arguments.
+    :param token: Token object.
+    :return boolean: True if authenticated else return False.
+    """
+    if args.get('auth') == token:
+        return True
+    else:
+        return False
+
+
+def extract_uuid_version_subscription_id(msg):
+    """
+    Extract uuid, version, subscription_id from message.
+    :param dict msg: A dictionary of message contains bundle information.
+    :return str uuid: uuid of the bundle.
+    :return str version: version of the bundle.
+    :return subscription_id: subscription id of the bundle.
+    """
+    uuid = msg["match"]["bundle_uuid"]
+    version = msg["match"]["bundle_version"]
+    subscription_id = msg["subscription_id"]
+    return uuid, version, subscription_id
+
+
+def compose_inputs(workflow_name, uuid, version):
+    """Compose workflow_name, uuid, version to a dictionary.
+    :param str workflow_name: The name of the workflow.
+    :param str uuid: uuid of the bundle.
+    :param str version: version of the bundle.
+    :return dict: A dictionary of workflow inputs.
+    """
+    return {workflow_name + '.bundle_uuid': uuid,
+            workflow_name + '.bundle_version': str(version)}
+
+
+def start_workflow(wdl_file, zip_file, inputs_file, inputs_file2, options_file, green_config):
+    """Use post method to call Cromwell API and get response.
+    :param str wdl_file: The path to wdl file.
+    :param str zip_file: The path to zip file.
+    :param str inputs_file: The path to inputs file.
+    :param str inputs_file2: The path to inputs file 2.
+    :param str options_file: The path to configs file.
+    :param green_config: The config class of current app.
+    :return response: HTTP response from cromwell.
+    """
+    with open(wdl_file, 'rb') as wdl, open(zip_file, 'rb') as deps, \
+            open(inputs_file, 'rb') as inputs, open(inputs_file2, 'rb') as inputs2, \
+            open(options_file, 'rb') as options:
+        files = {
+          'wdlSource': wdl,
+          'workflowInputs': inputs,
+          'workflowInputs_2': inputs2,
+          'wdlDependencies': deps,
+          'workflowOptions': options
+        }
+
+        response = requests.post(
+            green_config.cromwell_url, files=files,
+            auth=HTTPBasicAuth(green_config.cromwell_user,
+                               green_config.cromwell_password))
+        return response
+
+
+def download_gcs_blob(authenticated_gcs_client, bucket_name, source_blob_name, destination_file_name=None):
+    """Use google.cloud.storage API to download a blob from the bucket.
+    :param obj authenticated_gcs_client: An authenticated google cloud storage client object.
+    :param str bucket_name: A string of bucket name.
+    :param str source_blob_name: A string of source blob name that to be downloaded.
+    :param str destination_file_name: A string of destination file name.
+    """
+    if not destination_file_name:  # destination_file_name is set to source_blob_name by default
+        destination_file_name = './' + source_blob_name
+
+    if not isinstance(authenticated_gcs_client, storage.Client):
+        raise TypeError('Provided client is not a valid google cloud storage client object.')
+
+    try:
+        storage_client = authenticated_gcs_client  # pass the client as a parameter here
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(source_blob_name)
+        blob.download_to_filename(destination_file_name)
+        logging.info('Blob {0} downloaded to {1}.'.format(source_blob_name, destination_file_name))
+    except:
+        logging.debug(
+            'An error occurred during downloading blob {} from Google Cloud Storage'.format(source_blob_name))
---
+++ greenbox_tools/listener_utils/tests/test_utils.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import unittest
+import unittest.mock as mock
+from unittest.mock import call
+import requests_mock
+import json
+import requests
+from six.moves import http_client
+import tempfile
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+sys.path.insert(0, pkg_root)
+
+from greenbox_tools.listener_utils.listener_utils.utils import *
+
+
+def _make_credentials():
+    import google.auth.credentials
+
+    return mock.Mock(spec=google.auth.credentials.Credentials)
+
+
+def _make_response(status=http_client.OK, content=b'', headers={}):
+    response = requests.Response()
+    response.status_code = status
+    response._content = content
+    response.headers = headers
+    response.request = requests.Request()
+    return response
+
+
+def _make_json_response(data, status=http_client.OK, headers=None):
+    headers = headers or {}
+    headers['Content-Type'] = 'application/json'
+    return _make_response(
+        status=status,
+        content=json.dumps(data).encode('utf-8'),
+        headers=headers)
+
+
+def _make_requests_session(responses):
+    session = mock.create_autospec(requests.Session, instance=True)
+    session.request.side_effect = responses
+    return session
+
+
+class TestUtils(unittest.TestCase):
+
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.storage.client import Client
+
+        return Client
+
+    def _make_one(self, *args, **kw):
+        return self._get_target_class()(*args, **kw)
+
+    def test_get_filename_from_gs_link(self):
+        link = "gs://test_bucket_name/test_wdl_file.wdl"
+        self.assertEqual(get_filename_from_gs_link(link), 'test_wdl_file.wdl')
+
+    def test_parse_bucket_blob_from_gs_link_normal_link(self):
+        link = "gs://test_bucket_name/test_wdl_file.wdl"
+        bucket_name, blob_name = parse_bucket_blob_from_gs_link(link)
+        self.assertEqual(bucket_name, 'test_bucket_name')
+        self.assertEqual(blob_name, 'test_wdl_file.wdl')
+
+    def test_parse_bucket_blob_from_gs_link_special_link(self):
+        link = "gs://test_bucket_name/special/test/wdl/file.wdl"
+        bucket_name, blob_name = parse_bucket_blob_from_gs_link(link)
+        self.assertEqual(bucket_name, 'test_bucket_name')
+        self.assertEqual(blob_name, 'special/test/wdl/file.wdl')
+
+    def test_is_authenticated_no_auth_header(self):
+        self.assertFalse(is_authenticated({'foo': 'bar'}, 'baz'))
+
+    def test_is_authenticated_wrong_auth_value(self):
+        self.assertFalse(is_authenticated({'auth': 'bar'}, 'foo'))
+
+    def test_is_authenticated_valid(self):
+        self.assertTrue(is_authenticated({'auth': 'bar'}, 'bar'))
+
+    def test_extract_uuid_version_subscription_id(self):
+        body = {
+            'subscription_id': 222,
+            'match': {
+                'bundle_uuid': 'foo',
+                'bundle_version': 'bar'
+            }
+        }
+        uuid, version, subscription_id = extract_uuid_version_subscription_id(body)
+        self.assertEqual(uuid, 'foo')
+        self.assertEqual(version, 'bar')
+        self.assertEqual(subscription_id, 222)
+
+    def test_compose_inputs(self):
+        inputs = compose_inputs('foo', 'bar', 'baz')
+        self.assertEqual(inputs['foo.bundle_uuid'], 'bar')
+        self.assertEqual(inputs['foo.bundle_version'], 'baz')
+
+    @mock.patch("greenbox_tools.listener_utils.listener_utils.utils.open", create=True)
+    @requests_mock.mock()
+    def test_start_workflow(self, mock_open, mock_request):
+        """NOTE: This is just a workaround unittest, Integration test should be imported to further test!"""
+        wdl_file = "wdl_file"
+        zip_file = "zip_file"
+        inputs_file = "inputs_file"
+        inputs_file2 = "inputs_file2"
+        options_file = "options_file"
+        green_config = type('Config', (object,), {"cromwell_url": "http://cromwell_url",
+                                                  "cromwell_user": "cromwell_user",
+                                                  "cromwell_password": "cromwell_password"})
+
+        mock_open.side_effect = [
+            mock.mock_open(read_data="wdl_file").return_value,
+            mock.mock_open(read_data="zip_file").return_value,
+            mock.mock_open(read_data="inputs_file").return_value,
+            mock.mock_open(read_data="inputs_file2").return_value,
+            mock.mock_open(read_data="options_file").return_value
+        ]
+
+        file = {
+            'wdlSource': "wdl_file",
+            'workflowInputs': "inputs_file",
+            'workflowInputs_2': "inputs_file2",
+            'wdlDependencies': "zip_file",
+            'workflowOptions': "options_file"
+        }
+
+        def _request_callback(request, context):
+            context.status_code = 200
+            context.headers['test'] = 'header'
+            return {'request': {'body': "content"}}
+
+        # Check request actions
+        mock_request.post(green_config.cromwell_url, json=_request_callback)
+        result = start_workflow(wdl_file, zip_file, inputs_file, inputs_file2, options_file, green_config)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.headers.get('test'), 'header')
+
+        # Check file open actions
+        calls = [call(wdl_file, 'rb'), call(zip_file, 'rb'), call(inputs_file, 'rb'),
+                 call(inputs_file2, 'rb'), call(options_file, 'rb')]
+        mock_open.assert_has_calls(calls)
+
+    def test_download_gcs_blob_check_client(self):
+        from google.cloud.storage.bucket import Bucket
+
+        PROJECT = 'PROJECT'
+        CREDENTIALS = _make_credentials()
+        BUCKET_NAME = 'BUCKET_NAME'
+
+        client = self._make_one(project=PROJECT, credentials=CREDENTIALS)
+        bucket = client.bucket(BUCKET_NAME)
+        self.assertIsInstance(bucket, Bucket)
+        self.assertIs(bucket.client, client)
+        self.assertEqual(bucket.name, BUCKET_NAME)
+
+    def test_download_gcs_blob_hit(self):
+        PROJECT = 'PROJECT'
+        CREDENTIALS = _make_credentials()
+        BUCKET_NAME = 'BUCKET_NAME'
+
+        client = self._make_one(project=PROJECT, credentials=CREDENTIALS)
+        bucket = client.bucket(BUCKET_NAME)
+        blob_name = bucket.blob('test_blob')
+
+        with tempfile.NamedTemporaryFile() as dest_file:
+            download_gcs_blob(
+                client,
+                'BUCKET_NAME',
+                blob_name,
+                dest_file.name
+            )
+            assert os.path.exists(dest_file.name)  # NOTE: this is just a workaround, wait for integration test!
+
+    if __name__ == '__main__':
+        unittest.main()
