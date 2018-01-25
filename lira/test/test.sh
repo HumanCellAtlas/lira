dcf992979cbb19bb6da20cc744ec6c6edd7ab156
Rex Wang <rexwangcc@gmail.com>
2018-01-25 11:11:49 -0500

    Fix lira deployment Jenkins job  (#50)
    * Move listener deployment to ctmpl file.
    * Create kube-deploy.sh script for deploying to kubernetes.
    * Fix Jenkins Build job by adding a config parameter. (493)
    * Fix docker image by using Ubuntu 16.04 which is LTS. Add .dockerignore and add back kubernetes deploy YAML file. (493, 518)
    * Change docker tag used in the test. (493)

--- lira/test/test.sh
+++ lira/test/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t gcr.io/broad-dsde-mint-dev/lira:test ../..
+docker build -t lira:test ../..
 
 # Run unit tests in docker container
-docker run -e listener_config=config.json gcr.io/broad-dsde-mint-dev/lira:test bash -c "python -m unittest discover -v"
+docker run -e listener_config=config.json lira:test bash -c "python -m unittest discover -v"
