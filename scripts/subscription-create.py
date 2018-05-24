4705038a00f508b9e42bb7fc8e3016c998553ab9
Saman Ehsan <se8ea@virginia.edu>
2018-05-24 14:28:10 -0400

    Add attachments option to subscription
    Add option to pass in attachments when
    creating a subscription to the data
    store. This will include the specified
    fields in the matching notifications.

--- scripts/subscription-create.py
+++ scripts/subscription-create.py
@@ -6,16 +6,24 @@ from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def prep_json(callback_base_url, listener_secret, query_json):
-    js = {}
-    js["callback_url"] = "{0}?auth={1}".format(callback_base_url, listener_secret)
-
-    query = None
-    with open(query_json) as f:
+def prep_json(callback_base_url, listener_secret, query_json_file, attachments_file=None):
+    query = {}
+    with open(query_json_file) as f:
         query = json.load(f)
-    js["es_query"] = query
 
-    return js
+    payload = {
+        'es_query': query,
+        'callback_url': '{0}?auth={1}'.format(callback_base_url, listener_secret)
+    }
+
+    if attachments_file:
+        attachments = {}
+        with open(attachments_file) as f:
+            attachments = json.load(f)
+        payload['attachments'] = attachments
+
+    return payload
+
 
 def make_request(js, dss_url, key_file):
     scopes = ['https://www.googleapis.com/auth/userinfo.email']
@@ -25,6 +33,7 @@ def make_request(js, dss_url, key_file):
     response, content = h.request(dss_url, 'PUT', body=json.dumps(js), headers=headers)
     print(content)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("dss_url", help='Endpoint for creating new subscriptions in the storage service')
@@ -32,6 +41,9 @@ if __name__ == '__main__':
     parser.add_argument("listener_secret", help='Auth key for listener')
     parser.add_argument("key_file", help='JSON file containing storage service credentials')
     parser.add_argument("query_json", help='JSON file containing the query to register')
+    parser.add_argument("--additional_metadata",
+                        required=False,
+                        help='JSON file with additional fields to include in the notification')
     args = parser.parse_args()
-    js = prep_json(args.callback_base_url, args.listener_secret, args.query_json)
+    js = prep_json(args.callback_base_url, args.listener_secret, args.query_json, args.additional_metadata)
     make_request(js, args.dss_url, args.key_file)
--- scripts/subscription-create.sh
+++ scripts/subscription-create.sh
@@ -5,5 +5,10 @@ green_url=$2
 key_file=$3
 listener_secret=$4
 query_json=$5
+additional_metadata=$6
 
-./subscription-create.py "$dss_url?replica=gcp" $green_url $listener_secret $key_file $query_json
+if [ $additional_metadata ]; then
+    ./subscription-create.py "$dss_url?replica=gcp" $green_url $listener_secret $key_file $query_json --additional_metadata $additional_metadata
+else
+    ./subscription-create.py "$dss_url?replica=gcp" $green_url $listener_secret $key_file $query_json
+fi
