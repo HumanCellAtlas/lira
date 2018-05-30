62c0255401ae563b47dc9306e606d0c197b25202
Saman Ehsan <sehsan@broadinstitute.org>
2018-05-30 11:51:32 -0400

    Add option to include additional metadata in notifications (#77)
    * Add attachments option to subscription
    
    Add option to pass in attachments when
    creating a subscription to the data
    store. This will include the specified
    fields in the matching notifications.
    
    * Clean-up code

--- scripts/subscription-create.sh
+++ scripts/subscription-create.sh
@@ -5,5 +5,10 @@ green_url=$2
 key_file=$3
 listener_secret=$4
 query_json=$5
+additional_metadata=$6
 
-./subscription-create.py "$dss_url?replica=gcp" $green_url $listener_secret $key_file $query_json
+if [ $additional_metadata ]; then
+    additional_metadata_flag="--additional_metadata"
+fi
+
+./subscription-create.py "$dss_url?replica=gcp" $green_url $listener_secret $key_file $query_json $additional_metadata_flag $additional_metadata
