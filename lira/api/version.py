65ef8b7a5d24598747b99212dbcc15cc20232f01
Rex Wang <chengche@broadinstitute.org>
2018-06-29 11:10:00 -0400

    Update dependencies' version and refactor the version endpoint.

--- lira/api/version.py
+++ lira/api/version.py
@@ -6,10 +6,15 @@ from lira import lira_utils
 
 def get_version():
     """Collect and return Lira's and all its dependencies' versions."""
-    logger = logging.getLogger("{module_path}".format(module_path=__name__))
-    logger.debug("Version request received")
+    logger = logging.getLogger('{module_path}'.format(module_path=__name__))
+    logger.debug('Version request received')
 
     lira_config = current_app.config
+
+    try:
+        submit_wdl_version = lira_utils.parse_github_resource_url(lira_config.get('submit_wdl')).version or 'Unknown'
+    except ValueError:
+        submit_wdl_version = 'Unknown'
 
     workflow_info = {
         wdl.workflow_name: {
@@ -18,17 +23,26 @@ def get_version():
         } for wdl in lira_config.wdls
     }
 
-    try:
-        submit_wdl_version = lira_utils.parse_github_resource_url(lira_config.get('submit_wdl')).version or "Unknown"
-    except ValueError:
-        submit_wdl_version = "Unknown"
+    version_info = {
+        'cromwell_tools_version': cromwell_tools.__version__ or 'Unknown',
+        'lira_version': lira_config.version,
+        'submit_wdl_version': submit_wdl_version
+    }
+
+    settings_info = {
+        'cromwell_url': lira_config.get('cromwell_url'),
+        'data_store_url': lira_config.get('dss_url'),
+        'ingest_url': lira_config.get('ingest_url'),
+        'launch_time': current_app.launch_time,
+        'max_cromwell_retries': lira_config.get('max_cromwell_retries'),
+        'run_mode': 'dry_run' if lira_config.get('dry_run') else 'live_run',
+        'stale_notification_timeout': lira_config.get('stale_notification_timeout'),
+        'use_caas': lira_config.get('use_caas')
+    }
 
     version_response = {
-        "launch_time": current_app.launch_time,
-        "lira_version": lira_config.version,
-        "cromwell_tools_version": cromwell_tools.__version__ or "Unknown",
-        "submit_wdl_version": submit_wdl_version,
-        "run_mode": "dry_run" if lira_config.get('dry_run') else "live_run",
+        "settings_info": settings_info,
+        "version_info": version_info,
         "workflow_info": workflow_info
     }
 
