d93c0955d16e0aa2f9c7863eb726a9ebe2ec2ad5
rhiananthony <ranthony@broadinstitute.org>
2018-08-22 11:33:19 -0400

    Change variable for TLS cert from TLS_SECRET_1_NAME to TLS_SECRET_NAME. This was because we don't need to use 2 certs at present

--- kubernetes/deploy_tls_cert_and_ingress.sh
+++ kubernetes/deploy_tls_cert_and_ingress.sh
@@ -12,8 +12,8 @@ KUBERNETES_ZONE=${KUBERNETES_ZONE:-"us-central1-a"}
 
 TLS_FULL_CHAIN_DIR=${TLS_FULL_CHAIN_DIR:-"green-wildcard-ssl-certificate.crt"}
 TLS_PRIVATE_KEY_DIR=${TLS_PRIVATE_KEY_DIR:-"green-wildcard-ssl-certificate.key"}
-TLS_SECRET_1_NAME=${TLS_SECRET_1_NAME:-"hca-tls-secret"-$(date '+%Y-%m-%d-%H-%M-%S')}
-TLS_SECRET_2_NAME=${TLS_SECRET_2_NAME:-${TLS_SECRET_1_NAME}}
+TLS_SECRET_NAME=${TLS_SECRET_NAME:-"hca-tls-secret"-$(date '+%Y-%m-%d-%H-%M-%S')}
+TLS_SECRET_2_NAME=${TLS_SECRET_2_NAME:-${TLS_SECRET_NAME}}
 VAULT_TOKEN_PATH=${VAULT_TOKEN_PATH:-"${HOME}/.vault-token"}
 INGRESS_NAME=${INGRESS_NAME:-"lira-ingress"}
 SERVICE_NAME=${SERVICE_NAME:-"lira-service"}
@@ -46,12 +46,12 @@ gcloud container clusters get-credentials "${KUBERNETES_CLUSTER}" \
 
 echo "Creating TLS secret in Kubernetes"
 kubectl create secret tls \
-                "${TLS_SECRET_1_NAME}" \
+                "${TLS_SECRET_NAME}" \
                 --cert="${TLS_FULL_CHAIN_DIR}" \
                 --key="${TLS_PRIVATE_KEY_DIR}" \
                 --namespace="${KUBERNETES_NAMESPACE}"
 
-if [ "${TLS_SECRET_1_NAME}" != "${TLS_SECRET_2_NAME}" ];
+if [ "${TLS_SECRET_NAME}" != "${TLS_SECRET_2_NAME}" ];
 then
 kubectl create secret tls \
                 "${TLS_SECRET_2_NAME}" \
@@ -61,7 +61,7 @@ kubectl create secret tls \
 fi
 
 echo "Generating ingress file"
-docker run -i --rm -e TLS_SECRET_1_NAME="${TLS_SECRET_1_NAME}" \
+docker run -i --rm -e TLS_SECRET_NAME="${TLS_SECRET_NAME}" \
                    -e TLS_SECRET_2_NAME="${TLS_SECRET_2_NAME}" \
                    -e GLOBAL_IP_NAME="${GLOBAL_IP_NAME}" \
                    -e INGRESS_NAME="${INGRESS_NAME}" \
