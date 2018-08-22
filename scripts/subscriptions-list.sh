29e3c051839fa388d208c4948f1797158f796040
rhiananthony <ranthony@broadinstitute.org>
2018-08-22 11:29:48 -0400

    reverting three files which I had unwittingly checked in

--- scripts/subscriptions-list.sh
+++ scripts/subscriptions-list.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-ENV=dev
-DSS_URL=${DSS_URL:-"https://dss.${ENV}.data.humancellatlas.org/v1/subscriptions"}
-BLUEBOX_KEY_PATH="/Users/ranthony/GitHub/HumanCellAtlas/lira/kubernetes/bluebox-subscription-manager-dev-key.json"
-
-python3 subscriptions-list.py "${DSS_URL}?replica=gcp" ${BLUEBOX_KEY_PATH}
+source config.sh
+./subscriptions-list.py "$dss_url?replica=gcp" $key_file
