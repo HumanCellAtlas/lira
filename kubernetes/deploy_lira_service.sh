d4128a17e821a8b5078153c63807003fa93fa5b1
rhiananthony <ranthony@broadinstitute.org>
2018-09-04 15:13:39 -0400

    Adding step to get auth key from vault, then auth the gcloud project with it

--- kubernetes/deploy_lira_service.sh
+++ kubernetes/deploy_lira_service.sh
@@ -2,14 +2,25 @@
 
 # Variables
 GCLOUD_PROJECT=${GCLOUD_PROJECT:-"broad-dsde-mint-dev"} # other envs - broad-dsde-mint-test, broad-dsde-mint-staging, hca-dcp-pipelines-prod
-
 KUBERNETES_NAMESPACE=${KUBERNETES_NAMESPACE:-"green-100-us-central1-ns"}
 KUBERNETES_CLUSTER=${KUBERNETES_CLUSTER:-"green-100-us-central1"}
 KUBERNETES_ZONE=${KUBERNETES_ZONE:-"us-central1-a"}
-TLS_SECRET_NAME="${TLS_SECRET_NAME:-""}-$(date '+%Y-%m-%d')"
 VAULT_TOKEN_PATH=${VAULT_TOKEN_PATH:-"${HOME}/.vault-token"}
-APPLICATION_NAME=${APPLICATION_NAME:-"lira"}
-SERVICE_NAME=${SERVICE_NAME:-"lira-service"}
+
+APPLICATION_NAME="lira"
+SERVICE_NAME="lira-service"
+
+CAAS_KEY_PATH="secret/dsde/mint/${LIRA_ENVIRONMENT}/${SERVICE}/${CAAS_ENVIRONMENT}-key.json"
+
+echo "Retrieving service account key"
+docker run -i --rm \
+               -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+               -v "${PWD}":/working broadinstitute/dsde-toolbox:ra_rendering \
+               vault read -format=json "${CAAS_KEY_PATH}" | jq .data > "${CAAS_KEY_FILE}"
+
+echo "Authenticating with the service account"
+gcloud auth activate-service-account --key-file caas-prod-key.json
+
 
 echo "Getting kubernetes context"
 gcloud container clusters get-credentials "${KUBERNETES_CLUSTER}" \
