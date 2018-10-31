b7d6c06016944e78ae42bdf337c8da91daa32e59
rhiananthony <ranthony@broadinstitute.org>
2018-10-31 09:56:21 -0400

    Changing location of vault token

--- kubernetes/deploy_lira.sh
+++ kubernetes/deploy_lira.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VAULT_TOKEN_PATH=${VAULT_TOKEN_PATH:-"/etc/vault-token-dsde"}
+VAULT_TOKEN_PATH=${VAULT_TOKEN_PATH:-"/root/.vault-token"}
 
 #echo "PRINTING ENVIRONMENT VARIABLES"
 #env
