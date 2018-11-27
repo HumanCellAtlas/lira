d7b6eb40bd9ba74e18b94758512546f1517aacdb
rhiananthony <ranthony@broadinstitute.org>
2018-11-27 16:58:40 -0500

    Fixes

--- deploy/gitlab/generate_certs.sh
+++ deploy/gitlab/generate_certs.sh
@@ -4,19 +4,23 @@ echo "Getting AWS Users credentials from Vault"
 export AWS_ACCESS_KEY_ID="$(vault read -field="aws_access_key" secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/aws_cert_user)"
 export AWS_SECRET_ACCESS_KEY="$(vault read -field="aws_secret_key" secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/aws_cert_user)"
 
-export CERT_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/cert1.pem"
-export FULLCHAIN_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/fullchain1.pem"
-export PRIVKEY_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/privkey1.pem"
-export CHAIN_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/chain1.pem"
-
 echo "Making the temp directory for certs"
 mkdir certs
+
+cd certs
 
 echo "Writing certbot domain to file"
 echo ${DOMAIN} > certbot_domain.txt
 
 echo "Running certbot-route53 script"
 sh "${SCRIPTS_DIR}"/certbot-route53.sh
+
+cd ..
+
+export CERT_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/cert1.pem"
+export FULLCHAIN_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/fullchain1.pem"
+export PRIVKEY_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/privkey1.pem"
+export CHAIN_VAULT_DIR="${WORK_DIR}/certs/letsencrypt/archive/${DOMAIN}/chain1.pem"
 
 export VAULT_TOKEN="$(cat ${VAULT_WRITE_TOKEN_PATH})"
 
--- deploy/jenkins/generate_certs.sh
+++ deploy/jenkins/generate_certs.sh
@@ -3,19 +3,19 @@
 echo "Getting AWS Users credentials from Vault"
 AWS_ACCESS_KEY_ID="$(docker run -i \
                                 --rm \
-                                -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+                                -v "${VAULT_READ_TOKEN_PATH}":/root/.vault-token \
                                 -v "${PWD}":/working \
                                 broadinstitute/dsde-toolbox:ra_rendering \
                                 vault read -field="aws_access_key" secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/aws_cert_user)"
 AWS_SECRET_ACCESS_KEY="$(docker run -i \
                                     --rm \
-                                    -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+                                    -v "${VAULT_READ_TOKEN_PATH}":/root/.vault-token \
                                     -v "${PWD}":/working \
                                     broadinstitute/dsde-toolbox:ra_rendering \
                                     vault read -field="aws_secret_key" secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/aws_cert_user)"
 
 echo "Making the temp directory for certs"
-mkdir certs
+mkdir "certs"
 
 echo "Building the Certbot docker image"
 docker build -t certbot .
@@ -24,19 +24,29 @@ echo "Executing the certbot script to create a cert"
 docker run \
     -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
     -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
-    -v "${PWD}/certs":/working \
-    -v "${PWD}/certbot-route53.sh":/working/certbot-route53.sh \
+    -v $(pwd)/certs:/certs \
+    -v $(pwd)/certbot-route53.sh:/certs/certbot-route53.sh \
+    -w="/certs" \
+    --privileged \
     certbot:latest \
     bash -c \
-        "${SCRIPTS_DIR}"/scripts/certbot-route53.sh --agree-tos \
-                             --manual-public-ip-logging-ok \
-                             --email mintteam@broadinstitute.org \
-                             --domains "${DOMAIN}"
+        "sudo bash /certs/certbot-route53.sh \
+                --agree-tos \
+                --manual-public-ip-logging-ok \
+                --email mintteam@broadinstitute.org \
+                --domains ${DOMAIN}"
+
+CERT_VAULT_DIR="certs/letsencrypt/archive/${DOMAIN}/cert1.pem"
+FULLCHAIN_VAULT_DIR="certs/letsencrypt/archive/${DOMAIN}/fullchain1.pem"
+PRIVKEY_VAULT_DIR="certs/letsencrypt/archive/${DOMAIN}/privkey1.pem"
+CHAIN_VAULT_DIR="certs/letsencrypt/archive/${DOMAIN}/chain1.pem"
+
+sudo chown -R jenkins certs
 
 echo "Writing fullchain to vault at ${FULLCHAIN_VAULT_DIR}"
 docker run -i \
            --rm \
-           -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+           -v "${VAULT_WRITE_TOKEN_PATH}":/root/.vault-token \
            -v "${PWD}":/working \
            broadinstitute/dsde-toolbox:ra_rendering \
            vault write secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/fullchain.pem value=@"${FULLCHAIN_VAULT_DIR}"
@@ -44,7 +54,7 @@ docker run -i \
 echo "Writing privkey to vault at ${PRIVKEY_VAULT_DIR}"
 docker run -i \
            --rm \
-           -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+           -v "${VAULT_WRITE_TOKEN_PATH}":/root/.vault-token \
            -v "${PWD}":/working \
            broadinstitute/dsde-toolbox:ra_rendering \
            vault write secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/privkey.pem value=@"${PRIVKEY_VAULT_DIR}"
@@ -52,7 +62,7 @@ docker run -i \
 echo "Writing chain to vault at ${CHAIN_VAULT_DIR}"
 docker run -i \
            --rm \
-           -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+           -v "${VAULT_WRITE_TOKEN_PATH}":/root/.vault-token \
            -v "${PWD}":/working \
            broadinstitute/dsde-toolbox:ra_rendering \
            vault write secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/chain.pem value=@"${CHAIN_VAULT_DIR}"
@@ -60,7 +70,7 @@ docker run -i \
 echo "Writing cert to vault at ${CERT_VAULT_DIR}"
 docker run -i \
            --rm \
-           -v "${VAULT_TOKEN_PATH}":/root/.vault-token \
+           -v "${VAULT_WRITE_TOKEN_PATH}":/root/.vault-token \
            -v "${PWD}":/working \
            broadinstitute/dsde-toolbox:ra_rendering \
            vault write secret/dsde/mint/${LIRA_ENVIRONMENT}/lira/cert.pem value=@"${CERT_VAULT_DIR}"
