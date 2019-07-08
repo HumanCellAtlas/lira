232d1f6cdbee0f21a17c0debd9908b1d02645acf
Saman Ehsan <sehsan@broadinstitute.org>
2019-07-08 11:28:10 -0400

    Label every workflow with a hash of its inputs (#167)
    * Add input hash label
    
    Label SS2 and Optimus workflows
    with a hash of the sample_id,
    taxon_id, fastq file hashes
    and the workflow version
    
    * Fix encoding error
    
    * Fix returning the hash value
    
    * Remove workflow version from hash
    
    * Use all file hash types in hash-id
    
    Use the sha1, sha256, s3-etag and
    crc32c that the data store provides
    for each fastq file when creating the
    inputs hash.
    
    * Move duplicate input processing logic to pipeline-tools
    
    * Add test for bundle input hash
    
    * Minor style changes
    
    * Update requirements.txt
    
    * Update pip and setuptools version in travis
    
    * Fix flake8 errors
    
    * Update pipeline-tools version
    
    * Fix hashing inputs

---
+++ lira/bundle_inputs.py
@@ -0,0 +1,24 @@
+import hashlib
+from pipeline_tools.pipelines.smartseq2 import smartseq2
+from pipeline_tools.pipelines.optimus import optimus
+
+WORKFLOW_INPUTS = {
+    'AdapterSmartSeq2SingleCell': smartseq2.get_ss2_paired_end_inputs_to_hash,
+    'AdapterOptimus': optimus.get_optimus_inputs_to_hash,
+}
+
+
+def create_workflow_inputs_hash_label(
+    workflow_name, bundle_id, bundle_version, dss_url
+):
+    """
+    Create a hash out of the bundle-specific inputs for a workflow.
+    """
+    get_inputs_to_hash = WORKFLOW_INPUTS.get(workflow_name, None)
+    if get_inputs_to_hash:
+        workflow_inputs = get_inputs_to_hash(bundle_id, bundle_version, dss_url)
+
+        sha256_hash = hashlib.sha256(
+            bytes(''.join((str(i) for i in workflow_inputs)), encoding='utf-8')
+        )
+        return {'hash-id': sha256_hash.hexdigest()}
